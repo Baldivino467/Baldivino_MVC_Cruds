d73332cdfa0e582df9229ce87766079c981cc939
Reynante O. Baldivino <115506360+Baldivino467@users.noreply.github.com>
2025-03-05 20:25:52 +0800

    Delete mvc_baldivino directory

--- mvc_baldivino/resources/views/admin/students/name-column.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="d-flex align-items-center">
-    <div class="rounded-circle bg-primary text-white p-2 mr-2" style="width: 35px; height: 35px; display: flex; align-items: center; justify-content: center;">
-        {{ strtoupper(substr($student->name, 0, 1)) }}
-    </div>
-    {{ $student->name }}
-</div> 
